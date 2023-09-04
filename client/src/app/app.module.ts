a84d621d5e0eb55cc7316b3406834b361d5028dd
MadalinaRadulescu <radulescumadalina90@yahoo.com>
2023-09-04 12:03:57 +0300

    passing down data to child component

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -7,6 +7,7 @@ import {HttpClientModule} from "@angular/common/http";
 import {CoreModule} from "./core/core.module";
 import {ShopModule} from "./shop/shop.module";
 
+
 @NgModule({
   declarations: [
     AppComponent,
