3dd40d8cb042cb665b9aa216379c633eeefb0d06
MadalinaRadulescu <radulescumadalina90@yahoo.com>
2023-09-04 18:04:25 +0300

    search functionality added

--- client/src/app/shop/shop.module.ts
+++ client/src/app/shop/shop.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { ShopComponent } from './shop.component';
 import { ProductItemComponent } from './product-item/product-item.component';
 import {PaginationModule} from "ngx-bootstrap/pagination";
+import {SharedModule} from "../shared/shared.module";
 
 
 
@@ -16,7 +17,8 @@ import {PaginationModule} from "ngx-bootstrap/pagination";
   ],
   imports: [
     CommonModule,
-    PaginationModule
+    PaginationModule,
+    SharedModule
   ]
 })
 export class ShopModule { }
